cca27496b4e7883ffd085f8408bc0322fb497847
hilosync <62771262+hilosync@users.noreply.github.com>
2021-11-16 22:02:14 +0000

    v1, added tutorial

---
+++ Assets/Scripts/Tutorial.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Tutorial : MonoBehaviour
+{
+    [SerializeField] GameObject tutorialCanvas;
+    [SerializeField] Button CloseButton;
+    [SerializeField] Button OpenButton;
+
+    private void Awake() 
+    {
+        
+        CloseButton.onClick.RemoveAllListeners();
+        CloseButton.onClick.AddListener(HideTutorial);
+
+        OpenButton.onClick.RemoveAllListeners();
+        OpenButton.onClick.AddListener(ShowTutorial);
+    }
+
+    public void ShowTutorial()
+    {
+        tutorialCanvas.SetActive(true);
+    }
+
+    public void HideTutorial()
+    {
+        tutorialCanvas.SetActive(false);
+    }
+
+}
+
